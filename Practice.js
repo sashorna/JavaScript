209acaffc2fa22a2d386b6fe761c504e3f09aa6b
sashorna <sashorna018@gmail.com>
2024-09-30 17:22:48 +0600

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,9 @@
+//Q.1 Print all even numbers from 0 t0 100
+
+for ( let num = 0; num <= 100; num++) {
+    if (num%2 == 0){ //even number
+        console.log("num =", num);
+    }
+}
+
+//Q.2 Print all even numbers from 0 t0 100
